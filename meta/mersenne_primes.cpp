cc18df69e5e6fe2fa58d6de7216d0e6ccde86851
Eric <ericgrochowicz@hotmail.com>
2025-06-22 10:49:23 -0300

    add comments

--- meta/mersenne_primes.cpp
+++ meta/mersenne_primes.cpp
@@ -1,1 +1,2 @@
+// primos da forma 2^k - 1. alguns valores de k:
 vector<int> exp = {2,3,5,7,13,17,19,31,61,89,107,127};
--- meta/sieve.cpp
+++ meta/sieve.cpp
@@ -1,12 +1,13 @@
-int d[maxn], mu[maxn], phi[maxn];
+// crivo que computa quantidade de divisores, função de mobius e totiente em O(n log n)
+int d[N], mu[N], phi[N];
 vector<int> primes;
 void sieve() {
-    for(int i=1;i<maxn;i++) phi[i]=i, mu[i]=1;
+    for(int i=1;i<N;i++) phi[i]=i, mu[i]=1;
 
-    for(int i=2;i<maxn;i++) {
+    for(int i=2;i<N;i++) {
         if(!d[i]) {
             primes.push_back(i);
-            for(int j=i;j<maxn;j+=i) {
+            for(int j=i;j<N;j+=i) {
                 if(j>i) d[j]++;
                 if(j%(i*i)==0) mu[j] = 0;
                 mu[j] *= -1;
